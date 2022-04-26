355988d8e47effd6472ee1169fd166296cf8359b
Neringagaig <ngaigalaite@yahoo.com>
2022-04-26 20:10:21 +0300

    added two variables to script.js

---
+++ 1paskaita/script.js
@@ -0,0 +1,12 @@
+let myName;
+let myAge;
+// sukuriam kintamuosius
+myName = "Neringa"; 
+myAge = "24";
+//priskiriam kintamiejiem reikšmes
+
+//one line komentaras
+
+/*multi
+ line
+ komentaras */
