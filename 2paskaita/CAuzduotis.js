5c85fcc99019c03822c7a1522c380bf4771d177b
Neringagaig <ngaigalaite@yahoo.com>
2022-04-28 21:02:51 +0300

    added practice tasks

---
+++ 2paskaita/CAuzduotis.js
@@ -0,0 +1,30 @@
+// const clientAge = 24;
+// const legalAge = 20;
+// if (clientAge >= legalAge) {
+//   console.log(`Pasiekęs ${legalAge}`);
+// } else {
+//   console.log(`Nepasiekęs ${legalAge}`);
+// }
+
+// const name = 'Neringa';
+// if (name.length > 6) {
+//   console.log('Ilgas vardas');
+// }
+
+// const age = 24;
+// if (age > 100 || age < 0) {
+//   console.log('Invalid age');
+// } else if (age < 18) {
+//   console.log('Child');
+// } else {
+//   console.log('Adult')
+// }
+
+const car = "BMW";
+if (car === 'VW' || car === 'Audi' || car === 'Bentley' || car === 'Bugatti' || car === 'Lamborghini' || car === 'Porsche') {
+  console.log('VW Group');
+} else if (car === 'BMW' || car === 'Mini' || car === 'Rolls-Royce') {
+  console.log('BMW Group');
+} else {
+  console.log('Nei vienam');
+}
