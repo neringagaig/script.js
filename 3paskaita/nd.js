08897bcfec966c99ea2c20688501aebce5887ee3
Neringagaig <ngaigalaite@yahoo.com>
2022-05-03 17:18:11 +0300

    added js switch, ternary operator and for loop

---
+++ 3paskaita/nd.js
@@ -0,0 +1,111 @@
+//SWITCH FUNKCIJA
+
+// const car = "seat";
+
+// switch (car) {
+//     case "VW":
+//     case "Audi":
+//     case "Bentley":
+//     case "Bugatti":
+//     case "Lamborghini":
+//     case "Porsche":
+//         console.log ("VW Group");
+//     break;
+//     case "BMW":
+//     case "Mini":
+//     case "Rolls-Royce":
+//         console.log ("BMW Group");
+//     break;
+//     default:
+//         console.log ("none");
+// }
+
+// const userInput = "obuolys";
+
+// switch (userInput) {
+//     case "bananas":
+//     case "kriaušė":
+//     case "obuolys":
+//     case "melionas":
+//     case "apelsinas":
+//     console.log ("vaisius");
+//     break;
+//     case "morka":
+//     case "bulvė":
+//     case "burokas":
+//     case "agurkas":
+//     case "pomidoras":
+//     console.log ("daržovė");
+//     break;
+//     default:
+//         console.log ("nevalgoma");
+// }
+
+// const weekDay = 2;
+
+// switch (weekDay) {
+//     case 1:
+//         console.log ("pirmadienis");
+//     break;
+//     case 2:
+//         console.log ("antradienis");
+//     break;
+//     case 3:
+//         console.log ("trečiadienis");
+//     break;
+//     case 4: 
+//     console.log ("ketvirtadienis");
+//     break;
+//     case 5:
+//         console.log ("penktadienis");
+//     break;
+//     case 6:
+//         console.log ("šeštadienis");
+//     break;
+//     case 7:
+//         console.log ("sekmadienis");
+//         break;
+// }
+
+//TERNARY OPERATOR ONE-LINE OPERATOR
+
+// const name = "neringa";
+
+// const myName = 5 > name.length ? "short name" : " long name";
+
+// console.log (myName);
+
+// const clientAge = 125;
+// const legalAge = 18;
+// const drive = clientAge < 0 || clientAge > 120 ? console.log("invalid age") : legalAge >= clientAge ? console.log("can drive") : console.log("can't drive");
+
+
+// const phone = "iPhone";
+// const isIphoneUser = phone === "iPhone";
+// console.log (isIphoneUser);
+
+//FOR LOOPS
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Neringa");
+//   }
+// const name = "Neringa";
+// const count = 10;
+// for (let i = 0; i < count; i++) {
+//   console.log(name);
+// }
+
+// const name = "Neringa";
+// const count = 10;
+// for (let i = 0; i < count; i++) {
+//   console.log(`${i}. ${name}`);
+// }
+
+let i = 10;
+while(i > 0) {
+  console.log(i);
+  i--;
+}
+
+
+
---
+++ 3paskaita/script.js
@@ -0,0 +1,59 @@
+// || - or (arba)
+// && - and (ir)
+
+// console.log ("Trecia paskaita switch, temary operatoriai, for ciklas");
+
+// const key = 4;
+
+// switch (key) {
+//     case 1:
+//         console.log ("pirmadienis");
+//         break;
+//     case 2:
+//         console.log ("antradienis");
+//         break;
+//     case 3:
+//         console.log ("treciadienis");
+//         break;
+//     case 4:
+//         console.log ("ketvirtadies");
+//         break;
+//     case 5:
+//         console.log ("penktadienis");
+//         break;
+// }
+
+//SWITCH
+
+// const age = 25;
+
+// const legalAge = 20;
+// const drinkingAge = 20;
+// const celsiusDegrees = 26;
+
+// let result = "pilnametis";
+
+// if (age<legalAge) {
+//     result = "nepilnametis";
+// }
+//  if salygos patenkina else
+
+//TEMARY OPERATORS
+// const resultTernary = age <legalAge ? "nepilnametis" : "pilnametis";
+// const drink = age > drinkingAge ? "beer" : "milk";
+// const weather = celsiusDegrees <= 0 ? "cold" : celsiusDegrees <= 25 ? "warm" : "hot";
+// console.log(weather);
+// console.log(drink);
+// console.log(resultTernary);
+
+// termary symbol ? :
+// ? if
+//!! else
+
+const myName = "Neringa";
+const myLastname = "Gaigalaitė";
+
+console.log (myName + " " + myLastname);
+
+console.log(`${myName} ${myLastname}`);
+
