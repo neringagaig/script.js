1f00fe8f37d7d35e97ce32cd9b62bfff9f714506
Neringagaig <ngaigalaite@yahoo.com>
2022-05-17 16:21:15 +0300

    added 11 lesson

---
+++ 11paskaita/nd.js
@@ -0,0 +1,49 @@
+// const car = {
+//     doors: 5,
+//     color: "red",
+//     brand: "VW",
+// };
+
+// if(car.doors === 5) {
+//     alert("turi penkias duris");
+// } else if (car.doors === 4){
+//     alert("turi keturias duris");
+// }else {
+//     alert("turi neįprastą kiekį durų");
+// }
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", handleSubmitForm);
+
+// function handleSubmitForm(event) {
+//     const name = document.querySelectorAll("input[name = 'name']").value;
+//     const surname = document.querySelectorAll("input[name = 'surname']").value;
+//     const person = {
+//         name: name,
+//         surname: surname,
+//     };
+//     console.log(person);
+// }
+
+// const ageForm = document.querySelector("form");
+// ageForm.addEventListener("submit", handleAgeForm);
+
+// function handleAgeForm(event){
+//     event.preventDefault();
+//     const name = document.querySelector("input[name = 'name']").value;
+//     const age = document.querySelector("input[name = 'age']").value;
+
+//     let isLegalAge = age;
+
+//     if(age >= 18){
+//         console.log("true");
+//     } else {
+//         console.log ("false");
+//     }
+
+//     const person = {
+//         name: name,
+//         isLegalAge: isLegalAge,
+//     };
+//     console.log(person);
+// }
