741accb2e5d886ec6ececb07d016cf3a706940db
Neringagaig <ngaigalaite@yahoo.com>
2022-06-14 19:42:38 +0300

    ADD POST FOR TODO

--- 25paskaita/script.js
+++ 25paskaita/script.js
@@ -18,12 +18,49 @@ const renderTodo = (todo) => {
 fetch('https://jsonplaceholder.typicode.com/todos')
     .then(resp => resp.json())
     .then((response) => {
-        const todos = response.slice(0, 10);
+        const todos = response.slice(0, 5);
         //paimam 10 itemu is viso masyvo
         //rodo pirma objekta is masyvo
         todos.forEach((todo) => renderTodo(todo));
     })
     .catch((error) => {
-        console.error(error)
+        console.error(error);
     });
+
+//POST METODAS, NAUDJOAMAS KAI NORIMA ISSIUSTI DUOMENIS
+//FETCH KAI NORI GAUTI DUOMENIS
+const handleAddTodo = () => {
+    const todoText = document.querySelector('input[name="todo-text"]');
+    
+    const newTodo = {title: todoText.value, completed: false};
+
+    const params = {
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        //pakeiciam atgal formata, kad backendas suprastu kas yr kas
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }//tai yra parametras skirtas duomenu ikelimui i serveri, kuris rasosi prie fetcho
+
+fetch('https://jsonplaceholder.typicode.com/todos', params)
+    .then(resp => resp.json())
+    .then(response => {
+        renderTodo(newTodo);
+        todoText.value = "";
+        console.log(response)
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+
+    
+};
+
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", handleAddTodo);
+
+
+
+
       
